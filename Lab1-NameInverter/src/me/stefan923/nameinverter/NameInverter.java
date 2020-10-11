47c7aeaf7c837bbd266b047b8ad7a7f443c3b872
Stefan923 <stefanpopescu923@gmail.com>
2020-10-11 12:50:17 +0300

    Initial commit

---
+++ Lab1-NameInverter/src/me/stefan923/nameinverter/NameInverter.java
@@ -0,0 +1,45 @@
+package me.stefan923.nameinverter;
+
+import java.util.Scanner;
+
+public class NameInverter {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+
+        System.out.println(getInvertedName(name));
+    }
+
+    public static String getInvertedName(String name) {
+        if (name == null)
+            return null;
+
+        if (name.equals(""))
+            return null;
+
+        String[] args = name.split(" ");
+        if (args.length == 1)
+            return name;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean hasFirstName = false;
+        for (String string : args) {
+            if (string.equals("")) {
+                continue;
+            }
+
+            if (string.equalsIgnoreCase("Mr.") || string.equalsIgnoreCase("Mrs.") || string.equalsIgnoreCase("Phd.")) {
+                stringBuilder.append(" ").append(string);
+            } else if (!hasFirstName) {
+                stringBuilder.insert(0, stringBuilder.length() > 0 ? " " : "").insert(0, string);
+                hasFirstName = true;
+            } else {
+                stringBuilder.insert(0, ", ").insert(0, string);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
