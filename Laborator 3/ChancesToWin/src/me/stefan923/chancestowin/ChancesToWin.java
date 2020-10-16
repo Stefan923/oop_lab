c4228925ae706e3bd95d7cc45dbc8550cfbb93a0
Stefan923 <stefanpopescu923@gmail.com>
2020-10-16 22:20:16 +0300

    Implemented the formula for calculating the chance of winning

--- Laborator 3/ChancesToWin/src/me/stefan923/chancestowin/ChancesToWin.java
+++ Laborator 3/ChancesToWin/src/me/stefan923/chancestowin/ChancesToWin.java
@@ -1,2 +1,25 @@
-package me.stefan923.chancestowin;public class ChancesToWin {
+package me.stefan923.chancestowin;
+
+import java.math.BigInteger;
+
+public class ChancesToWin {
+
+    public static void main(String[] args) {
+        System.out.println("Sansele de castig la loto 6 din 49 sunt: 1 la " + calculate());
+    }
+
+    static BigInteger calculate() {
+        BigInteger chances = new BigInteger("1");
+
+        for (int i = 44; i <= 49; i++) {
+            chances = chances.multiply(BigInteger.valueOf(i));
+        }
+
+        for (int i = 2; i <= 6; i++) {
+            chances = chances.divide(BigInteger.valueOf(i));
+        }
+
+        return chances;
+    }
+
 }
