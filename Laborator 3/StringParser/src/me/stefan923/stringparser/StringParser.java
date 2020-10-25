c912e0575d27b6d53708b381343bc47a454aa382
Stefan923 <stefanpopescu923@gmail.com>
2020-10-25 15:00:50 +0200

    Printing input string

--- Laborator 3/StringParser/src/me/stefan923/stringparser/StringParser.java
+++ Laborator 3/StringParser/src/me/stefan923/stringparser/StringParser.java
@@ -1,2 +1,49 @@
-package me.stefan923.stringparser;public class StringParser {
+package me.stefan923.stringparser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringParser {
+
+    public static void main(String[] args) {
+        String string = "Ana are 12 mere si 2 pere.";
+        System.out.println("String: " + string);
+        System.out.println("Number of consonants: " + countConsonants(string));
+
+        Scanner scanner = new Scanner(System.in);
+        char vowel = scanner.next().charAt(0);
+        System.out.println("'" + vowel + "' indexes: " + getVowelIndexes(string, vowel).toString());
+    }
+
+    private static int countConsonants(String string) {
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (isConsonant(string.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static List<Integer> getVowelIndexes(String string, char character) {
+        string = string.toLowerCase();
+        character = Character.toLowerCase(character);
+        List<Integer> indexes = new ArrayList<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == character) {
+                indexes.add(i);
+            }
+        }
+
+        return indexes;
+    }
+
+    private static boolean isConsonant(char character) {
+        return Character.isLetter(character) && "aeiouAEIOU".indexOf(character) < 0;
+    }
+
 }
