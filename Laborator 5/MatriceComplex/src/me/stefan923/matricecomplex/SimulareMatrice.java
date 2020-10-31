343a746310a02aa4514748cf2d2076f43459ec0d
Stefan923 <stefanpopescu923@gmail.com>
2020-11-01 00:31:55 +0200

    Initial commit - simulator

---
+++ Laborator 5/MatriceComplex/src/me/stefan923/matricecomplex/SimulareMatrice.java
@@ -0,0 +1,35 @@
+package me.stefan923.matricecomplex;
+
+public class SimulareMatrice {
+
+    public static void main(String[] args) {
+        Matrice a = new Matrice(2, 3);
+        Matrice b = new Matrice(2, 3);
+        Matrice c = new Matrice(3, 3);
+        Complex[][] mA = new Complex[2][3];
+        Complex[][] mB = new Complex[2][3];
+        Complex[][] mC = new Complex[3][3];
+
+        mA[0][0] = mB[0][0] = mC[0][0] = new Complex(1, 1);
+        mA[0][1] = mB[0][1] = mC[0][1] = new Complex(1, 1);
+        mA[0][2] = mB[0][2] = mC[0][2] = new Complex(1, 1);
+        mA[1][0] = mB[1][0] = mC[1][0] = new Complex(1, 1);
+        mA[1][1] = mB[1][1] = mC[1][1] = new Complex(1, 1);
+        mA[1][2] = mB[1][2] = mC[1][2] = new Complex(1, 1);
+        mC[2][0] = new Complex(1, 1);
+        mC[2][1] = new Complex(1, 1);
+        mC[2][2] = new Complex(1, 1);
+
+        a.setValues(mA);
+        b.setValues(mB);
+        c.setValues(mC);
+
+        System.out.println("Matricea A:\n" + a.toString());
+        System.out.println("Matricea B:\n" + b.toString());
+        System.out.println("Matricea A + B:\n" + a.add(b).toString());
+        System.out.println("Matricea A - B:\n" + a.subtract(b).toString());
+        System.out.println("Matricea A * 5:\n" + a.multiply(5).toString());
+        System.out.println("Matricea A * C:\n" + a.multiply(c).toString());
+    }
+
+}
