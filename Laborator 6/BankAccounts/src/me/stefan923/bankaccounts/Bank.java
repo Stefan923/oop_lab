d01f211c50b6ccc441aa206670f721bd4c2d74e0
Stefan923 <stefanpopescu923@gmail.com>
2020-11-07 17:51:00 +0200

    Added methods for opening and closing accounts

--- Laborator 6/BankAccounts/src/me/stefan923/bankaccounts/Bank.java
+++ Laborator 6/BankAccounts/src/me/stefan923/bankaccounts/Bank.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class Bank {
 
-    private ArrayList<Account> accounts;
+    private int id;
+    private final ArrayList<Account> accounts;
 
     public Bank() {
+        id = 0;
         accounts = new ArrayList<>();
     }
 
@@ -26,6 +28,29 @@ public class Bank {
                 System.out.println(account.getAccountNumber() + " is in overdraft. (balance: " + account.getBalance() + ")");
             }
         });
+    }
+
+    public void openAccount() {
+        accounts.add(new Account(id));
+        System.out.println("A fost deschis un nou cont cu numarul \"" + id++ + "\".");
+    }
+
+    public void openSavingsAccount() {
+        accounts.add(new Account(id));
+        System.out.println("A fost deschis un nou cont cu numarul \"" + id++ + "\".");
+    }
+
+    public void openCurrentAccount() {
+        accounts.add(new Account(id));
+        System.out.println("A fost deschis un nou cont cu numarul \"" + id++ + "\".");
+    }
+
+    public void closeAccount(int number) {
+        if (accounts.removeIf(account -> account.getAccountNumber() == number)) {
+            System.out.println("Contul cu numarul " + number + " a fost inchis.");
+        } else {
+            System.out.println("Nu exista niciun cont cu numarul \"" + number + "\".");
+        }
     }
 
 }
