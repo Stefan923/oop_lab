7e22a6e40f38a35ee5af09cd471a21fb95cb87c9
Stefan923 <stefanpopescu923@gmail.com>
2020-11-07 16:14:04 +0200

    Added constructor

--- Laborator 6/Ghiozdan/src/me/stefan923/ghiozdan/Caiet.java
+++ Laborator 6/Ghiozdan/src/me/stefan923/ghiozdan/Caiet.java
@@ -2,6 +2,10 @@ package me.stefan923.ghiozdan;
 
 public class Caiet extends Rechizita {
 
+    public Caiet(String name) {
+        eticheta = name;
+    }
+
     @Override
     public String getName() {
         return "Caiet: " + eticheta;
--- Laborator 6/Ghiozdan/src/me/stefan923/ghiozdan/Manual.java
+++ Laborator 6/Ghiozdan/src/me/stefan923/ghiozdan/Manual.java
@@ -2,6 +2,10 @@ package me.stefan923.ghiozdan;
 
 public class Manual extends Rechizita {
 
+    public Manual(String name) {
+        eticheta = name;
+    }
+
     @Override
     public String getName() {
         return "Manual: " + eticheta;
