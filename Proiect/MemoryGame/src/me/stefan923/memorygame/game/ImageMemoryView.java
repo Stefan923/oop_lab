14aa0951a0f7baca81bf630edd50eb32cced7061
Stefan923 <stefanpopescu923@gmail.com>
2020-12-16 01:25:31 +0200

    Added a new JPanel for game board

--- Proiect/MemoryGame/src/me/stefan923/memorygame/game/ImageMemoryView.java
+++ Proiect/MemoryGame/src/me/stefan923/memorygame/game/ImageMemoryView.java
@@ -1,11 +1,45 @@
 package me.stefan923.memorygame.game;
 
-public class ImageMemoryView {
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageMemoryView extends JFrame {
 
     ImageMemoryModel model;
 
     public ImageMemoryView(ImageMemoryModel model) {
         this.model = model;
+
+        int boardSize = model.getSize();
+        JPanel gameBoard = new JPanel();
+        gameBoard.setLayout(new GridLayout(boardSize, boardSize));
+
+        JPanel content = new JPanel();
+        content.setLayout(new GridLayout(2, 2));
+        content.add(gameBoardToJPanel());
+
+        this.setContentPane(content);
+        this.pack();
+
+        this.setTitle("Memory Game");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private JPanel gameBoardToJPanel() {
+        Card[][] gameBoard = model.getGameBoard();
+        JPanel boardPanel = new JPanel();
+        boardPanel.setLayout(new GridLayout(gameBoard.length, gameBoard.length));
+
+        for (int i = 0; i < gameBoard.length; ++i) {
+            for (int j = 0; j < gameBoard[j].length; ++j) {
+                JButton button = new JButton(gameBoard[i][j].getImageIcon());
+                button.setContentAreaFilled(false);
+                boardPanel.add(button);
+            }
+        }
+
+        return boardPanel;
     }
 
 }
