da4b80cc4964de72967571eb803addce53cd817d
Stefan923 <stefanpopescu923@gmail.com>
2020-12-16 09:59:15 +0200

    Added the main class

---
+++ Proiect/MemoryGame/src/me/stefan923/memorygame/MemoryGame.java
@@ -0,0 +1,11 @@
+package me.stefan923.memorygame;
+
+import me.stefan923.memorygame.game.ImageMemoryMVC;
+
+public class MemoryGame {
+
+    public static void main(String[] args) {
+        new ImageMemoryMVC();
+    }
+
+}
