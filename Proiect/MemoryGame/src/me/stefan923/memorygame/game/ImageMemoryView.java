2454ed52ff71ce2c8e908d1829c1c4dfe3e24df8
Stefan923 <stefanpopescu923@gmail.com>
2020-12-16 09:36:42 +0200

    Added retry button and a text field for user's score

--- Proiect/MemoryGame/src/me/stefan923/memorygame/game/ImageMemoryView.java
+++ Proiect/MemoryGame/src/me/stefan923/memorygame/game/ImageMemoryView.java
@@ -7,6 +7,10 @@ public class ImageMemoryView extends JFrame {
 
     ImageMemoryModel model;
 
+    JTextField fieldScore = new JTextField();
+
+    JButton btnRetry = new StyledJButton("Retry").getButton();
+
     public ImageMemoryView(ImageMemoryModel model) {
         this.model = model;
 
@@ -14,9 +18,25 @@ public class ImageMemoryView extends JFrame {
         JPanel gameBoard = new JPanel();
         gameBoard.setLayout(new GridLayout(boardSize, boardSize));
 
+        JPanel panelScore = new JPanel();
+        panelScore.setLayout(new FlowLayout());
+        panelScore.add(fieldScore);
+        fieldScore.setText(String.valueOf(model.getScore()));
+        fieldScore.setEditable(false);
+
+        JPanel panelRetry = new JPanel();
+        panelScore.setLayout(new FlowLayout());
+        panelScore.add(btnRetry);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new GridLayout(2, 1));
+        rightPanel.add(panelScore);
+        rightPanel.add(panelRetry);
+
         JPanel content = new JPanel();
-        content.setLayout(new GridLayout(2, 2));
+        content.setLayout(new GridLayout(1, 2));
         content.add(gameBoardToJPanel());
+        content.add(rightPanel);
 
         this.setContentPane(content);
         this.pack();
