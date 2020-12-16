2bc2e23b3b479880fb2cf682d3e4f15c4f815a78
Stefan923 <stefanpopescu923@gmail.com>
2020-12-16 18:02:52 +0200

    Calling HomePageMVC() constructor instead of ImageMemoryMVC()

--- Proiect/MemoryGame/src/me/stefan923/memorygame/MemoryGame.java
+++ Proiect/MemoryGame/src/me/stefan923/memorygame/MemoryGame.java
@@ -1,11 +1,11 @@
 package me.stefan923.memorygame;
 
-import me.stefan923.memorygame.game.ImageMemoryMVC;
+import me.stefan923.memorygame.homepage.HomePageMVC;
 
 public class MemoryGame {
 
     public static void main(String[] args) {
-        new ImageMemoryMVC();
+        new HomePageMVC();
     }
 
 }
