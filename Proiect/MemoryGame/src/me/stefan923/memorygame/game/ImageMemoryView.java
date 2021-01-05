c859273f7f9de3803b13573927e0169d690985ef
Stefan923 <stefanpopescu923@gmail.com>
2021-01-06 00:27:48 +0200

    Changed right panel's design

--- Proiect/MemoryGame/src/me/stefan923/memorygame/game/ImageMemoryView.java
+++ Proiect/MemoryGame/src/me/stefan923/memorygame/game/ImageMemoryView.java
@@ -3,6 +3,7 @@ package me.stefan923.memorygame.game;
 import me.stefan923.memorygame.StyledJButton;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -24,22 +25,12 @@ public class ImageMemoryView extends JFrame {
         int boardSize = model.getSize();
         JPanel gameBoard = new JPanel();
         gameBoard.setLayout(new GridLayout(boardSize, boardSize));
-
-        JPanel rightPanel = new JPanel();
-        rightPanel.setLayout(new GridLayout(2, 1));
-        rightPanel.add(fieldScore);
-        rightPanel.add(btnRetry);
-        rightPanel.setMinimumSize(new Dimension(40, 0));
-
-        fieldScore.setText(String.valueOf(model.getScore()));
-        fieldScore.setEditable(false);
-
         loadBoardGame();
 
         JPanel content = new JPanel();
         content.setLayout(new FlowLayout());
         content.add(panelBoard);
-        content.add(rightPanel);
+        content.add(getButtonsPanel());
 
         this.setContentPane(content);
         this.pack();
@@ -48,6 +39,26 @@ public class ImageMemoryView extends JFrame {
         this.setLocation(dimension.width / 2 - this.getSize().width / 2,
                 dimension.height / 2 - this.getSize().height / 2);
         this.setTitle("Memory Game");
+    }
+
+    private JPanel getButtonsPanel() {
+        JPanel fieldScorePanel = new JPanel(new FlowLayout());
+        fieldScorePanel.add(fieldScore);
+        fieldScorePanel.setBorder(new EmptyBorder(0, 0, 5, 0));
+
+        JPanel btnRetryPanel = new JPanel(new FlowLayout());
+        btnRetryPanel.add(btnRetry);
+        btnRetryPanel.setBorder(new EmptyBorder(5, 0, 0, 0));
+
+        JPanel buttonsPanel = new JPanel(new GridLayout(2, 1));
+        buttonsPanel.add(fieldScorePanel);
+        buttonsPanel.add(btnRetryPanel);
+        buttonsPanel.setMinimumSize(new Dimension(40, 0));
+
+        fieldScore.setText(String.valueOf(model.getScore()));
+        fieldScore.setEditable(false);
+
+        return buttonsPanel;
     }
 
     private void loadBoardGame() {
